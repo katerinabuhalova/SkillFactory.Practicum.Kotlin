1127f9dcc57549febf727098da67000f978d76eb
Ekaterina <katsiarina.buhalova@gmail.com>
2021-12-01 14:49:56 +0300

    Added menu buttons; Inserted movie posters; Inserted text boxes; Applied Drawable for backgrounds; Applied CardView for rounded corners and levitation; Added functionality with displaying Toast messages at the click of each button.

--- app/src/main/java/com/awesomecompany/mykinopoisk/MainActivity.kt
+++ app/src/main/java/com/awesomecompany/mykinopoisk/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.awesomecompany.mykinopoisk
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_menu.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_LONG).show()
+        }
+        button2.setOnClickListener {
+            Toast.makeText(this, "Избранное", Toast.LENGTH_LONG).show()
+        }
+        button3.setOnClickListener {
+            Toast.makeText(this, "Подборки", Toast.LENGTH_LONG).show()
+        }
+        button4.setOnClickListener {
+            Toast.makeText(this, "Настройки", Toast.LENGTH_LONG).show()
+        }
+        button5.setOnClickListener {
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_LONG).show()
+        }
     }
 }
