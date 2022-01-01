b2b92f748c32fb8903bad4b90ffb76d5cbeb1eee
Ekaterina <katsiarina.buhalova@gmail.com>
2022-01-01 16:23:01 +0300

    implement RecyclerView

--- app/src/main/java/com/awesomecompany/mykinopoisk/MainActivity.kt
+++ app/src/main/java/com/awesomecompany/mykinopoisk/MainActivity.kt
@@ -4,14 +4,35 @@ import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.awesomecompany.mykinopoisk.data.Film
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var filmsAdapter: FilmListRecyclerAdapter
+
+    val filmsDataBase = listOf(
+        Film("Joker", R.drawable.joker, "Film1"),
+        Film("Titanic", R.drawable.titanic, "Film2"),
+        Film("Christmas Story", R.drawable.christmasstory, "Film3"),
+        Film("Car", R.drawable.car, "Film4"),
+        Film("Monsters", R.drawable.monstersinc24_500x749, "Film5"),
+        Film("Nemo", R.drawable.nemo, "Film6"),
+        Film("Ratatouille", R.drawable.ratatouille, "Film7"),
+        Film("Star Wars", R.drawable.picter3, "Film8")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initializeRecycler()
+
+    }
+
+    private fun appBarClickListener() {
         topAppBar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.settings -> {
@@ -21,7 +42,9 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
 
+    private fun bottomListener() {
         bottom_navigation.setOnNavigationItemSelectedListener {
 
             when (it.itemId) {
@@ -40,5 +63,27 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+
+    private fun initializeRecycler() {
+        main_recycler.apply {
+            //Инициализируем наш адаптер в конструктор передаем анонимно инициализированный интерфейс,
+            //оставим его пока пустым, он нам понадобится во второй части задания
+            filmsAdapter =
+                FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener {
+                    override fun click(film: Film) {
+                        TODO("Not yet implemented")
+                    }
+                })
+            //Присваиваем адаптер
+            adapter = filmsAdapter
+            //Присвои layoutmanager
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            //Применяем декоратор для отступов
+            val decorator = TopSpacingItemDecoration(8)
+            addItemDecoration(decorator)
+        }
+//Кладем нашу БД в RV
+        filmsAdapter.addItems(filmsDataBase)
     }
 }
---
+++ app/src/main/java/com/awesomecompany/mykinopoisk/data/Film.kt
@@ -0,0 +1,9 @@
+package com.awesomecompany.mykinopoisk.data
+
+data class Film(
+    val title: String,
+    val poster: Int,
+    val description: String
+)
+
+
