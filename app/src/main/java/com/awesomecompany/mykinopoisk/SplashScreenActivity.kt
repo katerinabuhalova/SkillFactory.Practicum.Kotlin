b4dab649da752c9b84d9c8afd56b8d9d4e580a66
Ekaterina <katsiarina.buhalova@gmail.com>
2022-02-10 21:54:59 +0300

    add animation to thr splashScreen

--- app/src/main/java/com/awesomecompany/mykinopoisk/SplashScreenActivity.kt
+++ app/src/main/java/com/awesomecompany/mykinopoisk/SplashScreenActivity.kt
@@ -1,9 +1,12 @@
 package com.awesomecompany.mykinopoisk
 
 import android.content.Intent
+import android.graphics.drawable.Animatable
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreenActivity : AppCompatActivity() {
 
@@ -12,6 +15,8 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        (music_note.drawable as Animatable).start()
+
         Handler().postDelayed(
             {
                 val i = Intent(this@SplashScreenActivity, MainActivity::class.java)
