18925dafa792745f454bf5ee60ae99b1d6d78a8a
Ekaterina <katsiarina.buhalova@gmail.com>
2022-04-14 15:32:37 +0300

    add to Dagger

--- app/src/main/java/com/awesomecompany/mykinopoisk/App.kt
+++ app/src/main/java/com/awesomecompany/mykinopoisk/App.kt
@@ -1,37 +1,17 @@
 package com.awesomecompany.mykinopoisk
 
 import android.app.Application
-import com.awesomecompany.mykinopoisk.data.ApiConstants
-import com.awesomecompany.mykinopoisk.data.TmdbApi
-import com.awesomecompany.mykinopoisk.domain.Interactor
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
+import com.awesomecompany.mykinopoisk.di.AppComponent
+import com.awesomecompany.mykinopoisk.di.DaggerAppComponent
 
 class App : Application() {
-    lateinit var interactor: Interactor
+    lateinit var dagger: AppComponent
 
     override fun onCreate() {
         super.onCreate()
         instance = this
-        val okHttpClient = OkHttpClient.Builder()
-            .callTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
-            .addInterceptor(HttpLoggingInterceptor().apply {
-                if (BuildConfig.DEBUG) {
-                    level = HttpLoggingInterceptor.Level.BASIC
-                }
-            })
-            .build()
-        val retrofit = Retrofit.Builder()
-            .baseUrl(ApiConstants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(okHttpClient)
-            .build()
-        val retrofitService = retrofit.create(TmdbApi::class.java)
-        interactor = Interactor(retrofitService)
+        dagger = DaggerAppComponent.create()
+
     }
 
     companion object {
---
+++ app/src/main/java/com/awesomecompany/mykinopoisk/data/ApiKey.kt
@@ -0,0 +1,5 @@
+package com.awesomecompany.mykinopoisk.data
+
+object ApiKey {
+    const val KEY = "41fed2aa069a1cd0703d15824da33141"
+}
